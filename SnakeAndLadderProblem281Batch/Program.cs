63037a81f807059e191c1ca67e6cfd5b03ced815
Shrikant <sawantshrikant07@gmail.com>
2023-05-15 10:53:37 +0530

    [Shrikant] add:PlayerStartAtPosition0

--- SnakeAndLadderProblem281Batch/Program.cs
+++ SnakeAndLadderProblem281Batch/Program.cs
@@ -1,9 +1,12 @@
 ﻿
+using SnakeAndLadderProblem281Batch;
 using System; 
 public class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome To Snake And Ladder!");
+        Start start = new Start();
+        start.PlaySnakeAndLadder();
     }
 }
---
+++ SnakeAndLadderProblem281Batch/Start.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProblem281Batch
+{
+    public class Start
+    {
+        private int playerPosition;
+        private Random random;
+
+        public Start()
+        {
+            playerPosition = 0;
+            random = new Random();
+        }
+
+        public void PlaySnakeAndLadder()
+        {
+            Console.WriteLine("Starting Snake and Ladder game...");
+            Console.WriteLine("Your starting position is {0}.", playerPosition);
+        }
+    }
+}
